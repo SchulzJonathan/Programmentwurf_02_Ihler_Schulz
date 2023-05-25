1eb23e9fa3c685832b095123e64ce40a2c939193
ihlermario <122440141+ihlermario@users.noreply.github.com>
2023-05-25 15:16:31 +0000

    initialize the first methods
    initialized the drink_coffe, get_name, get_alias method and started initializing the solve_problem method

---
+++ src/developer.cpp
@@ -0,0 +1,23 @@
+#include <developer.hpp>
+#include <iostream>
+#include <string>
+
+void Developer::drink_coffee()
+{
+    std::cout << "\033[31mShit the beer is empty. Well, then i have to drink a coffee.\033[0m\n";
+}
+
+auto Developer::get_name() -> std::string
+{
+    return name_;
+}
+
+auto Developer::get_alias() -> std::string
+{
+    return alias_;
+}
+
+void JuniorDeveloper::solve_problem()
+{
+    std::cout << "Thats too hard for me";
+}
