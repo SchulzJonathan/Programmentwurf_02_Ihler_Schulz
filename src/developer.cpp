c1d21e62977d2cef81fe6a1652ca7694f83476f5
SchulzJonathan <122674100+SchulzJonathan@users.noreply.github.com>
2023-05-26 08:05:30 +0000

    stream Operator << for std::cout << <Developer> now working
    Developers can now easily be printed using std::cout << <Developer>. This way of printing the developer was added to the solve_problem() method along with some minor design changes in the prints

--- src/developer.cpp
+++ src/developer.cpp
@@ -4,7 +4,7 @@
 
 void Developer::drink_coffee()
 {
-    std::cout << "\033[31mShit the beer is empty. Well, then i have to drink a coffee.\033[0m\n";
+    std::cout << "\033[31mShit the beer is empty. Well, then i have to drink a coffee.\033[0m" << std::endl;
 }
 
 auto Developer::get_name() -> std::string
@@ -17,31 +17,33 @@ auto Developer::get_alias() -> std::string
     return alias_;
 }
 
+// overloading the << operator, so the Developer class can be printed using std::cout << <Developer>;
+std::ostream &operator<<(std::ostream &printthis, Developer &developertoprint)
+{
+    printthis << "Name:  " << developertoprint.get_name() << std::endl;
+    printthis << "Alias: " << developertoprint.get_alias();
+    return printthis;
+}
+
 void JuniorDeveloper::solve_problem()
 {
-    std::cout << "\033[31m=============================================================\033[0m\n" << std::endl;
+    std::cout << "\033[31m=============================================================\033[0m" << std::endl;
     std::cout << "Solving a Problem:" << std::endl;
-    //<< this;
+    std::cout << *this << std::endl;
     std::cout << "That was pretty hard. Next time this should do someone else" << std::endl;
     Developer::drink_coffee();
+    std::cout << "\033[31m=============================================================\033[0m" << std::endl;
 }
 
 void SeniorDeveloper::solve_problem()
 {
-    std::cout << "\033[31m=============================================================\033[0m\n" << std::endl;
+    std::cout << "\033[31m=============================================================\033[0m" << std::endl;
     std::cout << "Solving a Problem:" << std::endl;
-    // std::cout << this << std::endl;
+    std::cout << *this << std::endl;
     std::cout << "Nothing could be easier." << std::endl;
     Developer::drink_coffee();
+    std::cout << "\033[31m=============================================================\033[0m" << std::endl;
 }
-
-// std::ostream &operator<< (std::ostream &os, SeniorDeveloper & abc)
-// {
-//     std::string cdf = abc.get_name();
-//     os << cdf;
-//     //stream << "Alias: " << abc.get_alias() << std::endl;
-//     return os;
-// }
 
 Developer::Developer(const std::string name, const std::string alias)
 {
