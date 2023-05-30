803c9671496a05d10826c590f2ca42e35297339a
ihlermario <122440141+ihlermario@users.noreply.github.com>
2023-05-30 12:44:28 +0000

    changed the dynamic memory allocation handling
    sharepointers are now created when objects get instanziated

--- src/main.cpp
+++ src/main.cpp
@@ -8,11 +8,11 @@ auto main() -> int
     // This is the array with the developers inside
     std::vector<std::shared_ptr<Developer>> developer_group;
     // initialising Developers
-    SeniorDeveloper sen_dev("Diana Prince", "Wonder Woman");
-    JuniorDeveloper jun_dev("Peter Parker", "Spiderman");
+    std::shared_ptr<SeniorDeveloper> sen_dev = std::make_shared<SeniorDeveloper>("Diana Prince", "Wonder Woman");
+    std::shared_ptr<JuniorDeveloper> jun_dev = std::make_shared<JuniorDeveloper>("Peter Parker", "Spiderman");
     // Adding our Developers to our vector
-    developer_group.push_back(std::make_shared<SeniorDeveloper>(sen_dev));
-    developer_group.push_back(std::make_shared<JuniorDeveloper>(jun_dev));
+    developer_group.push_back(sen_dev);
+    developer_group.push_back(jun_dev);
     // Let the develolpers solve some problems
     for (auto& current_dev : developer_group)
     {
