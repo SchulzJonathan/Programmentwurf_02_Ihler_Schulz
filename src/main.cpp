701078eb24637c1d2b9516032c8a7186f974c987
ihlermario <122440141+ihlermario@users.noreply.github.com>
2023-06-01 09:54:11 +0000

    fixed the dynamic cast
     sen_dev and jun_dev now get castet to a base class when they get written into the vector

--- src/main.cpp
+++ src/main.cpp
@@ -11,14 +11,12 @@ auto main() -> int
     std::shared_ptr<SeniorDeveloper> sen_dev = std::make_shared<SeniorDeveloper>("Diana Prince", "Wonder Woman");
     std::shared_ptr<JuniorDeveloper> jun_dev = std::make_shared<JuniorDeveloper>("Peter Parker", "Spiderman");
     // Adding our Developers to our vector
-    developer_group.push_back(sen_dev);
-    developer_group.push_back(jun_dev);
-    // Let the develolpers solve some problems and cast them to its base class 
+    developer_group.push_back(std::dynamic_pointer_cast<Developer>(sen_dev));
+    developer_group.push_back(std::dynamic_pointer_cast<Developer>(jun_dev));
+    
+    // Let the develolpers solve some problems and cast them to its base class
     for (auto& current_dev : developer_group)
     {
-        if (auto base_dev = std::dynamic_pointer_cast<Developer>(current_dev))
-        {
-            base_dev->solve_problem();  
-        }
+        current_dev -> solve_problem();
     };
 }
